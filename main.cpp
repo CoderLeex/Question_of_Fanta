4218640877ff4f24f7c9b3fe531f36e23d70e0c4
CoderLeex <42692181+CoderLeex@users.noreply.github.com>
2018-08-25 17:08:11 +0800

    sourcefiles
    my first upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int num=0;
+void move(int n, char a, char b, char c)
+{
+	if (n == 1)
+	{
+		printf("%c-->%c\n",a,c);
+		num++;
+		return;
+	}
+	move(n - 1, a, c, b);
+	printf("%c-->%c\n", a, c);
+	num++;
+	move(n - 1, b, a, c);
+}
+void main()
+{
+	//num = 0;
+	move(4, 'A', 'B', 'C');
+	cout << "num:" << num << endl;
+	/*for (int i = 1; i <= 100;)
+	{
+		cout << i << endl;
+		i += 3;
+	}*/
+	system("pause");
+}
